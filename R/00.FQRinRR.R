91e8b795f4d25a1861c507f1a56fab7f71bd4a49
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2021-11-25 14:50:10 +0000

    adding spatial structure generation

--- R/00.FQRinRR.R
+++ R/00.FQRinRR.R
@@ -1,5 +1,117 @@
+## this file includes the analysis to generate the FQR in RR estimates
 library(here)
 library(data.table)
+
+
+## this is in two parts
+## part 1 generaters a nearest neighbour structure
+## part 2 does the resampling to generate the FQR in RR estimates
+## part 1 is included for reproducibility really
+## the nearest neighbour structure is included in repo/indata so part 2 can be run without running part 1
+runpart1 <- FALSE #run part 1 too?
+
+if(runpart1){
+
+  ## === part 1: generate spatial structure  ===
+  library(tmap)
+  library(rworldmap)
+  library(spdep)
+
+
+  ## Checking the countries and constructing adjacencies
+  ## Load spatial data and compare with covariates
+  load(here('indata/isoidx.Rdata')) #country list
+  data(countriesCoarse)                   #load rworldmap data
+  names(countriesCoarse@data)[30] <- 'iso3' #rename
+
+
+  ## Remove Antarctica and Greenland (ATA, GRL)
+  WTB <- countriesCoarse
+  WTB <- WTB[which(!(WTB@data$iso3 %in% c('ATA','GRL'))),]
+  WTB <- WTB[which(WTB@data$iso3 %in% isoidx),] #drops BES (Bonnaire) and TKL (Tokelau)
+  dim(WTB)
+
+
+  ## Start making adjacencies
+  W.nb <- poly2nb(WTB,row.names=as.character(WTB@data$iso3))
+  W.list <- nb2listw( W.nb,style='B',zero.policy = TRUE)
+
+  ## look at
+  plot(W.nb,coordinates(WTB),add=TRUE,col=2)
+
+
+  ## Generate naive nearest neighbout maps
+  cs_kn5 <- knn2nb(knearneigh(coordinates(WTB),k=5))
+
+
+  ## look at the $k=5$ version
+  plot(WTB,col='gray');text(coordinates(WTB),labels=WTB$iso3);plot(cs_kn5,coordinates(WTB),add=TRUE,col=2);
+
+
+  ## Modifying the nearest neighbour linkages
+  ## 
+  ## The naive maps have some features that are undesirable. In particular;
+  ## 
+  ## * they are not connected, which can hamper mixing
+  ## * Russia is not connected to former Soviet republics
+  ##
+  ## To address these issues, we introduce transatlantic links based on flight paths carrying more than 900K per year. We also join up the South Pacific islands WLF and TON to FJI. Finally, we join RUS to EST, LVA, UKR, GEO and AZE, which makes sense both in geographical and political terms.
+  ##
+  ## This function is to carry out these changes and to save the data and plots out.
+
+  appendKNN <- function(X,savename){
+    ## connect up RUS to FSRs & consider making connected
+    toadd <- c('EST','LVA','UKR','GEO','AZE')
+    toadd <- which(WTB$iso3 %in% toadd)
+    indiso <- which(WTB$iso3=='RUS')
+    X[[indiso]] <- c(X[[indiso]],toadd)
+    ## transatlantic links see: https://en.wikipedia.org/wiki/Transatlantic_flight#Busiest_transatlantic_routes
+    ## those with >900K 
+    ## transatlantic links UK, FRA to US 
+    toadd <- c('GBR','FRA')
+    toadd <- which(WTB$iso3 %in% toadd)
+    indiso <- which(WTB$iso3=='USA')
+    X[[indiso]] <- c(X[[indiso]],toadd)
+    ## UK to CAN
+    toadd <- which(WTB$iso3 == 'GBR')
+    indiso <- which(WTB$iso3=='CAN')
+    X[[indiso]] <- c(X[[indiso]],toadd)
+    ## US to ETH
+    toadd <- which(WTB$iso3 == 'ETH')
+    indiso <- which(WTB$iso3=='USA')
+    X[[indiso]] <- c(X[[indiso]],toadd)
+    ## BRA to AGO
+    toadd <- which(WTB$iso3 == 'BRA')
+    indiso <- which(WTB$iso3=='AGO')
+    X[[indiso]] <- c(X[[indiso]],toadd)
+    ## plot
+    pdf(paste0(savename,'.pdf'),w=18,h=12)
+    plot(WTB,col='gray')
+    plot(X, coordinates(WTB),add=TRUE,col=2)
+    text(coordinates(WTB),labels=WTB$iso3)
+    text(x=0,y=-60,labels='Not shown: WLF & TON connected to FJI')
+    dev.off()
+    ## transatlantic links WLF, TON to FJI (don't map)
+    toadd <- c('WLF','TON')
+    toadd <- which(WTB$iso3 %in% toadd)
+    indiso <- which(WTB$iso3=='FJI')
+    X[[indiso]] <- c(X[[indiso]],toadd)
+    save(X,file = paste0(savename,'.Rdata'))
+    X
+  }
+
+
+  ## apply this function:
+  acs_kn5 <- appendKNN(cs_kn5,here('data/acs_kn5'))
+
+  ## copying to indata so that the above doesn't need to be run
+  file.copy(here('data/acs_kn5.Rdata'),here('indata/acs_kn5.Rdata'))
+
+} #end of part 1
+
+## === part 2: use spatial structure for generating a sample of FQR in RR ===
+## load 5 nearest country structure (generated above)
+load(here('indata/acs_kn5.Rdata'))
 
 ## read in WHO DS data
 RS <- fread(here('indata/TB_dr_surveillance_2020-10-15.csv'))
@@ -26,9 +138,6 @@ RSR <- RS[,.(rr_N=sum(rr_N),f_n=sum(f_n)),
           by=.(g_whoregion)] #regional aggregate
 
 
-## load 5 nearest country structure
-load(here('indata/acs_kn5.Rdata'))
-load(here('indata/isoidx.Rdata'))
 
 
 ## make a data structure containing neighbouts
