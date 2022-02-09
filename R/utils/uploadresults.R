cc35b18c2963a30ed676796b3f83bc0a6e252fe6
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2022-02-09 11:12:09 +0000

    uploader dealing with discount rate SA

--- R/utils/uploadresults.R
+++ R/utils/uploadresults.R
@@ -31,3 +31,29 @@ upload.to.sheets <- function(filename,sheetid,
 upload.to.sheets("table_resources.csv",shid,pulmsa)
 upload.to.sheets("table_outcomes.csv",shid,pulmsa)
 upload.to.sheets("table_HE.csv",shid,pulmsa)
+
+
+## --- gather & write discount rate SAs
+L <- list()
+for(vr in c("","1","5")){
+  tmp <- fread(glue(here('output/table_HE{vr}.csv')))
+  L[[vr]] <- tmp[nrow(tmp)]
+}
+dsctsa <- rbindlist(L)
+dsctsa[,c('variable','none'):=NULL]
+dsctsa[,`discount rate`:=c('3%','1%','5%')]
+setcolorder(dsctsa,c('discount rate',names(dsctsa)[1:(ncol(dsctsa)-1)]))
+
+dscr <- melt(dsctsa,id='discount rate')
+dscr[,c('target group','TPT regimen'):=tstrsplit(variable,"_")]
+dscr <- dscr[,.(`target group`,`TPT regimen`,`discount rate`,ICER=value)]
+
+
+write_sheet( dsctsa,shid,sheet="dsctSA")
+write_sheet(dscr,shid,sheet="dsctSA2")
+
+## quick check of ratios
+dscr[,ref:=value[1],by=.(`target group`,`TPT regimen`)]
+dscr[,ratio:=as.numeric(value)/as.numeric(ref)]
+dscr[,mean(ratio),by=`discount rate`] #0.57 & 1.52
+
